1c9f27eefaa7859950e50ec8bd41fb21e8383173
Asad Meeran <100802771+ajasad25@users.noreply.github.com>
2026-02-17 23:59:54 +0500

    resolving final dependencies issues

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
+import './global.css';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { store } from './src/store/store';
-import RootNavigator from './src/app/RootNavigator';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
